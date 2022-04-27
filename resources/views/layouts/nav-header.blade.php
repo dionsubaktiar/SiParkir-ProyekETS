3a3f6f4752e0e0ccb5422e4df4a92a8a7a6375b7
dionsubaktiar <104469933+dionsubaktiar@users.noreply.github.com>
2022-04-27 21:48:43 +0700

    Add All Files

---
+++ resources/views/layouts/nav-header.blade.php
@@ -0,0 +1,15 @@
+<nav class="main-header navbar navbar-expand navbar-white navbar-light">
+ <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+            <a class="dropdown-item" href="{{ route('logout') }}"
+               onclick="event.preventDefault();
+                             document.getElementById('logout-form').submit();">
+                {{ __('Logout') }}
+            </a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                @csrf
+            </form>
+      </li>
+    </ul>
+  </nav>
